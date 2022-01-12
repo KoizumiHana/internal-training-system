442c0be33e30acca9ac976d62e74b0edfac17045
mayorov <mayorov@rit.va>
2022-01-12 15:19:29 +0300

    #2 Init training service folder structure.

---
+++ training/src/main/kotlin/com/service/trainings/training/TrainingApplication.kt
@@ -0,0 +1,11 @@
+package com.service.trainings.training
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TrainingApplication
+
+fun main(args: Array<String>) {
+    runApplication<TrainingApplication>(*args)
+}
