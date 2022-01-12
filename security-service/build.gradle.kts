a4ccbdc723f337467f394b069afddf32de1bcf59
kazimov <kazimov@rit.va>
2022-01-12 17:18:31 +0300

    delete lombok dependency and remove HELP.md

--- security-service/build.gradle.kts
+++ security-service/build.gradle.kts
@@ -26,8 +26,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
